9d41625c11e14695b569289a63baf39d26d215d3
Johannes Ringler <johannesringler@gmail.com>
2024-06-09 19:55:39 +0200

    can actually spawn platforms added in blender

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use bevy::prelude::*;
+use bevy_gltf_components::ComponentsFromGltfPlugin;
+use bevy_registry_export::ExportRegistryPlugin;
+
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+struct Platform {
+    test: i32,
+}
+
+fn load_level(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("Scene.glb#Scene0"),
+        ..Default::default()
+    });
+}
+
+fn print_platforms(query: Query<&Platform>) {
+    println!("Platforms:");
+    for platform in query.iter() {
+        println!("{:?}", platform);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .register_type::<Platform>()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(ExportRegistryPlugin::default())
+        .add_plugins(ComponentsFromGltfPlugin::default())
+        .add_systems(Startup, load_level)
+        .add_systems(Update, print_platforms)
+        .run();
 }
