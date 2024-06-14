69771859392d1f84c1f3d19290ca11b5f7aec0c7
Johannes Ringler <johannesringler@gmail.com>
2024-06-14 22:54:29 +0200

    experiment with joints

--- src/player.rs
+++ src/player.rs
@@ -7,22 +7,29 @@ pub struct Player {
     test: i32,
 }
 
+#[derive(Component, Debug)]
+struct PlayerSpawned;
+
 #[derive(Component, Reflect, Debug)]
 struct PlayerFeet;
 
 fn spawn_player(
     mut commands: Commands,
-    query: Query<(Entity, &Transform), (With<Player>, Without<RigidBody>)>,
+    query: Query<(Entity, &Transform), (With<Player>, Without<PlayerSpawned>)>,
 ) {
     for (entity, transform) in query.iter() {
+        commands.entity(entity).insert(PlayerSpawned);
         println!("Spawning player: {:?}", entity);
-        commands.entity(entity).insert((
-            Collider::capsule(2.0, 1.0),
-            RigidBody::default(),
-            Name::new("Player"),
-        ));
+        let body = commands
+            .spawn((
+                Collider::capsule(2.0, 1.0),
+                RigidBody::default(),
+                Name::new("PlayerBody"),
+                Position::from(transform.translation),
+            ))
+            .id();
         let mut feet_transform = transform.clone();
-        feet_transform.translation.y -= 1.5;
+        feet_transform.translation.y -= 2.5;
         println!("transform: {:?}", feet_transform);
         let feet = commands
             .spawn((
@@ -33,11 +40,19 @@ fn spawn_player(
                 Position::from(feet_transform.translation),
             ))
             .id();
-        let mut joint = DistanceJoint::new(entity, feet)
+        let mut spring = DistanceJoint::new(body, feet)
             .with_rest_length(4.5)
-            .with_linear_velocity_damping(14.0);
-        joint.compliance = 0.001;
-        commands.spawn(joint);
+            .with_linear_velocity_damping(0.0);
+        spring.compliance = 0.001;
+        // commands.spawn((spring, Name::new("PlayerSpring")));
+        let mut prism = PrismaticJoint::new(body, feet).with_free_axis(Vec3::Y);
+        prism.compliance = 0.0005;
+        // commands.spawn(prism);
+        let mut fixed = FixedJoint::new(body, feet)
+            .with_local_anchor_1(-Vec3::Y * 3.5)
+            .with_angular_velocity_damping(100.0);
+        fixed.compliance = 0.001;
+        commands.spawn(fixed);
     }
 }
 
@@ -47,6 +62,7 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<Player>();
         app.register_type::<DistanceJoint>();
+        app.insert_resource(SubstepCount(12));
         app.add_systems(Update, spawn_player);
     }
 }
