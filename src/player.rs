9520048914856a43d1a5101f6e1ec074f59b4d8b
Johannes Ringler <johannesringler@gmail.com>
2024-06-17 00:42:28 +0200

    add input system and attempt for lean torque

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,22 @@
 use bevy::prelude::*;
 use bevy_xpbd_3d::{prelude::*, SubstepSchedule, SubstepSet};
+use leafwing_input_manager::prelude::*;
+
+#[derive(Actionlike, PartialEq, Eq, Hash, Clone, Debug, Reflect)]
+pub enum Action {
+    Jump,
+    Move,
+    View,
+}
+
+impl Action {
+    fn default_input_map() -> InputMap<Self> {
+        let mut input_map = InputMap::default();
+        input_map.insert(Self::Move, VirtualDPad::wasd());
+        input_map.insert(Self::Jump, KeyCode::Space);
+        input_map
+    }
+}
 
 #[derive(PhysicsLayer)]
 pub enum Layer {
@@ -29,6 +46,17 @@ struct PlayerGroundSpring {
     damping: f32,
 }
 
+#[derive(Component, Reflect, Debug)]
+struct PlayerAngularSpring {
+    stiffness: f32,
+    damping: f32,
+}
+
+#[derive(Component, Reflect, Debug, Default)]
+struct PlayerMoveState {
+    acc_dir: Vec3,
+}
+
 fn spawn_player(
     mut commands: Commands,
     query: Query<(Entity, &Transform), (With<PlayerSpawn>, Without<PlayerSpawned>)>,
@@ -45,13 +73,32 @@ fn spawn_player(
                 Position::from(transform.translation),
                 Player,
                 ExternalForce::default().with_persistence(false),
+                ExternalTorque::default().with_persistence(false),
                 PlayerGroundSpring {
                     rest_length: 2.0,
                     stiffness: 1000.0,
                     damping: 100.0,
                 },
+                PlayerAngularSpring {
+                    stiffness: 100.0,
+                    damping: 10.0,
+                },
+                InputManagerBundle::<Action>::with_map(Action::default_input_map()),
+                PlayerMoveState::default(),
             ))
             .id();
+    }
+}
+
+fn player_controls(mut query: Query<(&ActionState<Action>, &mut PlayerMoveState), With<Player>>) {
+    for (action_state, mut move_state) in query.iter_mut() {
+        let dir = action_state
+            .clamped_axis_pair(&Action::Move)
+            .unwrap()
+            .xy()
+            .normalize_or_zero();
+        println!("Move: {:?}", dir);
+        move_state.acc_dir = dir.extend(0.0);
     }
 }
 
@@ -60,15 +107,31 @@ fn update_ground_force(
         (
             &RigidBody,
             &mut ExternalForce,
+            &mut ExternalTorque,
             &Position,
+            &Rotation,
             &LinearVelocity,
+            &AngularVelocity,
             &PlayerGroundSpring,
+            &PlayerAngularSpring,
+            &PlayerMoveState,
         ),
         With<Player>,
     >,
     shape_cast: SpatialQuery,
 ) {
-    for (body, mut force, Position(position), LinearVelocity(velocity), spring) in query.iter_mut()
+    for (
+        body,
+        mut force,
+        mut torque,
+        Position(position),
+        Rotation(quat),
+        LinearVelocity(velocity),
+        AngularVelocity(angular_vel),
+        spring,
+        angular_spring,
+        move_state,
+    ) in query.iter_mut()
     {
         if let Some(coll) = shape_cast.cast_shape(
             &Collider::sphere(1.0),
@@ -79,14 +142,21 @@ fn update_ground_force(
             false,
             SpatialQueryFilter::from_mask(Layer::Platform),
         ) {
-            println!("Time {:?}", coll.time_of_impact);
+            // println!("Time {:?}", coll.time_of_impact);
             force.set_force(
                 (-spring.stiffness * (coll.time_of_impact - spring.rest_length)
                     - spring.damping * velocity.y)
                     * Vec3::Y,
             );
-            println!("Force {:?}", force.force());
+            // println!("Force {:?}", force.force());
         }
+        let yaw = move_state.acc_dir.y.atan2(move_state.acc_dir.x);
+        let pitch = move_state.acc_dir.length();
+        let target_quat = Quat::from_euler(EulerRot::YXZ, yaw, pitch, 0.0);
+        let delta_quat = target_quat * quat.inverse();
+        let spring_torque =
+            -angular_spring.stiffness * delta_quat.to_axis_angle().1 * delta_quat.to_axis_angle().0;
+        torque.set_torque(spring_torque);
     }
 }
 
@@ -94,10 +164,11 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
+        app.add_plugins(InputManagerPlugin::<Action>::default());
         app.register_type::<PlayerSpawn>();
         app.register_type::<DistanceJoint>();
         app.insert_resource(SubstepCount(12));
-        app.add_systems(Update, spawn_player);
+        app.add_systems(Update, (spawn_player, player_controls));
         app.add_systems(
             SubstepSchedule,
             update_ground_force.before(SubstepSet::Integrate),
