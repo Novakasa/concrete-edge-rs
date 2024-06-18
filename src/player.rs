93ae1ddbbc3c4aab10745500bd4a02f64c85cf63
Johannes Ringler <johannesringler@gmail.com>
2024-06-18 15:03:12 +0200

    add camera tracking, movement controls kinda working

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,9 @@
-use bevy::prelude::*;
-use bevy_xpbd_3d::{prelude::*, SubstepSchedule, SubstepSet};
+use bevy::{prelude::*, transform::TransformSystem};
+use bevy_xpbd_3d::{math::PI, prelude::*, SubstepSchedule, SubstepSet};
 use leafwing_input_manager::prelude::*;
+
+const CAPSULE_RADIUS: f32 = 0.2;
+const CAPSULE_HEIGHT: f32 = 4.0 * CAPSULE_RADIUS;
 
 #[derive(Actionlike, PartialEq, Eq, Hash, Clone, Debug, Reflect)]
 pub enum Action {
@@ -60,34 +63,70 @@ struct PlayerMoveState {
 
 fn spawn_player(
     mut commands: Commands,
-    query: Query<(Entity, &Transform), (With<PlayerSpawn>, Without<PlayerSpawned>)>,
+    query: Query<(Entity, &GlobalTransform), (With<PlayerSpawn>, Without<PlayerSpawned>)>,
 ) {
     for (entity, transform) in query.iter() {
         commands.entity(entity).insert(PlayerSpawned);
         println!("Spawning player: {:?}", entity);
         let body = commands
             .spawn((
-                Collider::capsule(2.0, 1.0),
+                Collider::capsule(CAPSULE_RADIUS * 2.0, CAPSULE_RADIUS),
                 CollisionLayers::new(Layer::Player, Layer::Platform),
                 RigidBody::default(),
                 Name::new("PlayerBody"),
-                Position::from(transform.translation),
+                Position::from(transform.translation()),
+                Transform::from_translation(transform.translation()),
+                GlobalTransform::default(),
                 Player,
                 ExternalForce::default().with_persistence(false),
                 ExternalTorque::default().with_persistence(false),
                 PlayerGroundSpring {
-                    rest_length: 2.0,
-                    stiffness: 3000.0,
-                    damping: 100.0,
+                    rest_length: 0.0,
+                    stiffness: 300.0,
+                    damping: 20.0,
                 },
                 PlayerAngularSpring {
-                    stiffness: 1000.0,
-                    damping: 100.0,
+                    stiffness: 10.0,
+                    damping: 2.0,
                 },
                 InputManagerBundle::<Action>::with_map(Action::default_input_map()),
                 PlayerMoveState::default(),
             ))
             .id();
+    }
+}
+
+#[derive(Component, Reflect, Debug)]
+struct CameraAnchor;
+
+fn spawn_camera(mut commands: Commands) {
+    let camera_arm = 0.25 * Vec3::new(0.0, 8.0, 25.0);
+    let transform =
+        Transform::from_translation(camera_arm).looking_to(-camera_arm.normalize(), Vec3::Y);
+    commands
+        .spawn((
+            TransformBundle::default(),
+            Name::new("CameraAnchor"),
+            CameraAnchor,
+        ))
+        .with_children(|builder| {
+            builder
+                .spawn(Camera3dBundle {
+                    transform: transform,
+                    ..Default::default()
+                })
+                .insert(Name::new("PlayerCamera"));
+        });
+}
+
+fn track_camera(
+    query: Query<(&GlobalTransform), With<Player>>,
+    mut camera_query: Query<&mut Transform, With<CameraAnchor>>,
+) {
+    for (player_transform) in query.iter() {
+        for mut transform in camera_query.iter_mut() {
+            transform.translation = player_transform.translation().clone();
+        }
     }
 }
 
@@ -101,9 +140,9 @@ fn player_controls(mut query: Query<(&ActionState<Action>, &mut PlayerMoveState)
         println!("Move: {:?}", dir);
         move_state.acc_dir = Vec3::new(dir.x, 0.0, dir.y);
         if action_state.pressed(&Action::Jump) {
-            move_state.spring_height = 2.0;
+            move_state.spring_height = CAPSULE_HEIGHT * 0.7;
         } else {
-            move_state.spring_height = 3.0;
+            move_state.spring_height = CAPSULE_HEIGHT * 1.3;
         }
     }
 }
@@ -111,7 +150,6 @@ fn player_controls(mut query: Query<(&ActionState<Action>, &mut PlayerMoveState)
 fn update_ground_force(
     mut query: Query<
         (
-            &RigidBody,
             &mut ExternalForce,
             &mut ExternalTorque,
             &Position,
@@ -125,9 +163,9 @@ fn update_ground_force(
         With<Player>,
     >,
     shape_cast: SpatialQuery,
+    mut gizmos: Gizmos,
 ) {
     for (
-        body,
         mut force,
         mut torque,
         Position(position),
@@ -141,14 +179,20 @@ fn update_ground_force(
     {
         let from_up = *quat * Vec3::Y;
         if let Some(coll) = shape_cast.cast_shape(
-            &Collider::sphere(1.0),
+            &Collider::sphere(CAPSULE_RADIUS),
             position.clone(),
             Quat::default(),
             Direction3d::new_unchecked(-from_up),
-            4.0,
+            CAPSULE_HEIGHT * 2.0,
             false,
             SpatialQueryFilter::from_mask(Layer::Platform),
         ) {
+            gizmos.sphere(
+                position.clone() + -from_up * coll.time_of_impact,
+                Quat::IDENTITY,
+                CAPSULE_RADIUS,
+                Color::RED,
+            );
             let contact_point = coll.point2 + -from_up * coll.time_of_impact;
             // println!("Time {:?}", coll.time_of_impact);
             let spring_force = (-spring.stiffness
@@ -160,16 +204,34 @@ fn update_ground_force(
             // println!("Force {:?}", force.force());
             let yaw = move_state.acc_dir.z.atan2(move_state.acc_dir.x);
             let pitch = move_state.acc_dir.length();
-            let target_quat = Quat::from_euler(EulerRot::YZX, yaw, -0.2 * pitch, 0.0);
+            let target_quat = Quat::from_euler(EulerRot::YZX, yaw, -0.2 * PI * pitch, 0.0);
             let target_up = target_quat * Vec3::Y;
             let delta_angle = from_up.angle_between(target_up);
             let delta_axis = from_up.cross(target_up).normalize_or_zero();
             //println!("From up dir: {:?}, Target up dir: {:?}", from_up, target_up);
             // println!("Delta angle: {:?}", delta_angle);
             let spring_torque = angular_spring.stiffness * delta_axis * delta_angle
-                - (angular_spring.damping * angular_vel.clone());
+                - (angular_spring.damping * angular_vel.clone())
+                - force.torque();
+            let cm_force = -(spring_torque + force.torque()).cross(contact_point);
+            if false {
+                if cm_force.dot(target_up.cross(from_up)) > 0.0 && false {
+                    println!("accelerating");
+                    force.apply_force(cm_force);
+                } else {
+                    println!("decelerating");
+                    force.apply_force(cm_force);
+                }
+            }
             torque.set_torque(spring_torque);
-            force.apply_force(-spring_torque.cross(contact_point));
+            let acc_dir = Vec3::Y.cross(spring_torque).normalize_or_zero();
+            let dir_cross_contact = acc_dir.cross(contact_point);
+            if dir_cross_contact.length() > 0.00001 {
+                let f_acc = acc_dir * (spring_force * Vec3::Y.cross(contact_point)).length()
+                    / (dir_cross_contact.length());
+                println!("Force {:?}", f_acc);
+                force.apply_force(f_acc);
+            }
         }
     }
 }
@@ -181,8 +243,18 @@ impl Plugin for PlayerPlugin {
         app.add_plugins(InputManagerPlugin::<Action>::default());
         app.register_type::<PlayerSpawn>();
         app.register_type::<DistanceJoint>();
-        app.insert_resource(SubstepCount(4));
-        app.add_systems(Update, (spawn_player, player_controls));
+        app.insert_resource(SubstepCount(12));
+        app.add_systems(Startup, spawn_camera);
+        app.add_systems(
+            Update,
+            (
+                spawn_player,
+                player_controls,
+                track_camera
+                    .after(PhysicsSet::Sync)
+                    .before(TransformSystem::TransformPropagate),
+            ),
+        );
         app.add_systems(
             SubstepSchedule,
             update_ground_force.before(SubstepSet::Integrate),
