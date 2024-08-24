74a94638d0bf619b21ffd1855b64e62a2701bdda
Johannes Ringler <johannesringler@gmail.com>
2024-08-24 14:55:47 +0200

    split player physics into new module

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -11,53 +11,12 @@ use bevy::{
 };
 use dynamics::integrator::IntegrationSet;
 use leafwing_input_manager::prelude::*;
+use physics::{
+    PhysicsDebugInfo, PlayerAngularSpring, PlayerGroundSpring, PlayerMoveState, CAPSULE_HEIGHT,
+    CAPSULE_RADIUS, CAST_RADIUS,
+};
 
-const CAPSULE_RADIUS: f32 = 0.2;
-const CAPSULE_HEIGHT: f32 = 4.0 * CAPSULE_RADIUS;
-const CAST_RADIUS: f32 = 1.0 * CAPSULE_RADIUS;
-const MAX_TOI: f32 = CAPSULE_HEIGHT * 1.0;
-const FRICTION_MARGIN: f32 = 0.9;
-const GLOBAL_FRICTION: f32 = 1.0;
-
-#[derive(Clone)]
-enum SpringParams {
-    Physical { stiffness: f32, damping: f32 },
-    Effective { f: f32, zeta: f32, m: f32 },
-}
-
-impl SpringParams {
-    fn get_physical(&self) -> Self {
-        match self {
-            Self::Physical {
-                stiffness: _,
-                damping: _,
-            } => self.clone(),
-            Self::Effective { f, zeta, m } => Self::Physical {
-                stiffness: (2.0 * PI * f).powi(2) * m,
-                damping: zeta * f * m / PI,
-            },
-        }
-    }
-}
-
-#[derive(Component, Reflect, Debug, Clone, Default)]
-struct SpringValue {
-    f: f32,
-    zeta: f32,
-    m: f32,
-    velocity: f32,
-    value: f32,
-}
-
-impl SpringValue {
-    fn update(&mut self, target: f32, dt: f32) -> f32 {
-        let k = (2.0 * PI * self.f).powi(2) * self.m;
-        let c = self.zeta * self.f * self.m / PI;
-        self.velocity += k * (target - self.value) * dt - c * self.velocity * dt;
-        self.value += self.velocity * dt;
-        self.value
-    }
-}
+mod physics;
 
 #[derive(States, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum DebugState {
@@ -120,55 +79,6 @@ struct PlayerFeet;
 #[derive(Component, Reflect, Debug)]
 pub struct Player;
 
-#[derive(Component, Reflect, Debug, Resource, Clone, Default)]
-#[reflect(Component, Resource)]
-struct PlayerGroundSpring {
-    rest_length: f32,
-    stiffness: f32,
-    min_damping: f32,
-    max_damping: f32,
-    max_force: f32,
-    min_force: f32,
-    smoothing_spring: SpringValue,
-}
-
-impl PlayerGroundSpring {
-    fn force(&mut self, length: f32, vel: f32, normal: Vec3, dt: f32) -> f32 {
-        let damping = self
-            .max_damping
-            .lerp(self.min_damping, normal.dot(Vec3::Y).abs());
-        let target = (-self.stiffness * (length - self.rest_length).min(0.0) - damping * vel)
-            .min(self.max_force)
-            .max(self.min_force);
-        self.smoothing_spring.update(target, dt);
-        target
-    }
-}
-
-#[derive(Component, Reflect, Debug, Resource, Clone, Default)]
-#[reflect(Component, Resource)]
-struct PlayerAngularSpring {
-    stiffness: f32,
-    damping: f32,
-    turn_stiffness: f32,
-}
-
-#[derive(Component, Reflect, Debug, Default)]
-struct PlayerMoveState {
-    acc_dir: Vec3,
-    spring_height: f32,
-    prev_vel: Vec3,
-    prev_angular_force: Vec3,
-    prev_target_force: Vec3,
-    neg_cast_vec: Vec3,
-    slipping: bool,
-    contact_point: Option<Vec3>,
-    contact_normal: Option<Vec3>,
-    current_force: Vec3,
-    ext_force: Vec3,
-    forward_dir: Vec3,
-}
-
 #[derive(Debug, Default)]
 struct FootTravelInfo {
     time: f32,
@@ -214,31 +124,11 @@ struct ProceduralRigState {
     foot_states: [FootState; 2],
 }
 
-#[derive(Component, Reflect, Debug, Default)]
-struct PhysicsDebugInfo {
-    grounded: bool,
-    spring_force: Vec3,
-    spring_torque: Vec3,
-    normal_force: Vec3,
-    tangential_force: Vec3,
-    shape_toi: f32,
-    cast_dir: Vec3,
-    spring_dir: Vec3,
-    contact_point: Vec3,
-    position: Vec3,
-    angle_force: Vec3,
-    ground_normal: Vec3,
-    tangent_vel: Vec3,
-    target_vel: Vec3,
-    target_force: Vec3,
-    delta_quat: Quat,
-}
-
 fn spawn_player(
     mut commands: Commands,
     query: Query<(Entity, &GlobalTransform), (With<PlayerSpawn>, Without<PlayerSpawned>)>,
-    ground_spring: Res<PlayerGroundSpring>,
-    angular_spring: Res<PlayerAngularSpring>,
+    ground_spring: Res<physics::PlayerGroundSpring>,
+    angular_spring: Res<physics::PlayerAngularSpring>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     debug_state: Res<State<DebugState>>,
@@ -247,8 +137,8 @@ fn spawn_player(
         commands.entity(entity).insert(PlayerSpawned);
         println!("Spawning player: {:?}", entity);
         let capsule = meshes.add(Mesh::from(Capsule3d::new(
-            CAPSULE_RADIUS,
-            CAPSULE_HEIGHT - 2.0 * CAPSULE_RADIUS,
+            physics::CAPSULE_RADIUS,
+            physics::CAPSULE_HEIGHT - 2.0 * physics::CAPSULE_RADIUS,
         )));
         let material = materials.add(Color::WHITE);
         let visibility = if debug_state.get() == &DebugState::None {
@@ -258,7 +148,10 @@ fn spawn_player(
         };
         let _body = commands
             .spawn((
-                Collider::capsule(CAPSULE_RADIUS, CAPSULE_HEIGHT - 2.0 * CAPSULE_RADIUS),
+                Collider::capsule(
+                    physics::CAPSULE_RADIUS,
+                    physics::CAPSULE_HEIGHT - 2.0 * physics::CAPSULE_RADIUS,
+                ),
                 ColliderDensity(1.5),
                 CollisionLayers::new(Layer::Player, Layer::Platform),
                 RigidBody::default(),
@@ -271,11 +164,8 @@ fn spawn_player(
                 ground_spring.clone(),
                 angular_spring.clone(),
                 InputManagerBundle::<PlayerAction>::with_map(PlayerAction::default_input_map()),
-                PlayerMoveState {
-                    neg_cast_vec: Vec3::Y,
-                    ..Default::default()
-                },
-                PhysicsDebugInfo::default(),
+                physics::PlayerMoveState::new(),
+                physics::PhysicsDebugInfo::default(),
             ))
             .insert(MaterialMeshBundle {
                 mesh: capsule,
@@ -330,7 +220,7 @@ fn spawn_camera_3rd_person(mut commands: Commands) {
 }
 
 fn spawn_camera_1st_person(mut commands: Commands) {
-    let camera_arm = Vec3::new(0.0, 0.2 * CAPSULE_HEIGHT, 0.0);
+    let camera_arm = Vec3::new(0.0, 0.2 * physics::CAPSULE_HEIGHT, 0.0);
     let transform = Transform::from_translation(camera_arm);
     commands
         .spawn((
@@ -394,7 +284,7 @@ fn track_camera_1st_person(
 ) {
     for (Position(pos), Rotation(quat)) in query.iter() {
         let up_dir = *quat * Vec3::Y;
-        let pos = pos.clone() + up_dir * CAPSULE_HEIGHT * 0.3;
+        let pos = pos.clone() + up_dir * physics::CAPSULE_HEIGHT * 0.3;
         for (mut transform, cam1) in camera_query.iter_mut() {
             let view_unrolled = Quat::from_euler(EulerRot::YXZ, cam1.yaw, cam1.pitch, 0.0);
             let forward = view_unrolled * Vec3::NEG_Z;
@@ -425,9 +315,9 @@ fn player_controls(
     mut query: Query<
         (
             &ActionState<PlayerAction>,
-            &mut PlayerMoveState,
-            &mut PlayerGroundSpring,
-            &mut PlayerAngularSpring,
+            &mut physics::PlayerMoveState,
+            &mut physics::PlayerGroundSpring,
+            &mut physics::PlayerAngularSpring,
         ),
         With<Player>,
     >,
@@ -465,197 +355,6 @@ fn player_controls(
                 angular_spring.damping = 0.2;
                 angular_spring.turn_stiffness = 0.4;
             }
-        }
-    }
-}
-
-fn update_ground_force(
-    mut query: Query<
-        (
-            &mut ExternalForce,
-            &mut ExternalTorque,
-            &Position,
-            &Rotation,
-            &LinearVelocity,
-            &AngularVelocity,
-            &mut PlayerGroundSpring,
-            &PlayerAngularSpring,
-            &mut PlayerMoveState,
-            &mut PhysicsDebugInfo,
-        ),
-        With<Player>,
-    >,
-    shape_cast: SpatialQuery,
-    dt: Res<Time<Substeps>>,
-    gravity: Res<Gravity>,
-) {
-    for (
-        mut force,
-        mut torque,
-        Position(position),
-        Rotation(quat),
-        LinearVelocity(velocity),
-        AngularVelocity(angular_vel),
-        mut spring,
-        angular_spring,
-        mut move_state,
-        mut debug,
-    ) in query.iter_mut()
-    {
-        let external_forces = gravity.0;
-        let ext_dir = external_forces.normalize_or_zero();
-        let filter = SpatialQueryFilter::from_mask(Layer::Platform);
-        let cast_dir = -move_state.neg_cast_vec;
-        let capsule_up = *quat * Vec3::Y;
-        let capsule_right = *quat * Vec3::X;
-        move_state.forward_dir = *quat * Vec3::NEG_Z;
-        if let Some(coll) = shape_cast.cast_shape(
-            &Collider::sphere(CAST_RADIUS),
-            position.clone(),
-            Quat::IDENTITY,
-            Dir3::new_unchecked(cast_dir.normalize_or_zero()),
-            MAX_TOI,
-            false,
-            filter.clone(),
-        ) {
-            let normal = coll.normal1;
-            debug.ground_normal = normal;
-            move_state.contact_normal = Some(normal);
-
-            let contact_point = coll.point2 + cast_dir * coll.time_of_impact;
-            move_state.contact_point = Some(contact_point);
-            let spring_dir = -contact_point.normalize_or_zero();
-            // let spring_dir = from_up;
-            debug.contact_point = contact_point;
-            debug.shape_toi = coll.time_of_impact;
-            debug.cast_dir = cast_dir;
-
-            let spring_vel = -velocity.dot(normal) / (-spring_dir.dot(normal));
-            // println!("Time {:?}", coll.time_of_impact);
-            let spring_force = spring.force(
-                contact_point.length(),
-                spring_vel,
-                normal,
-                dt.delta_seconds(),
-            ) * spring_dir;
-            let grounded = spring_force.length() > 0.0001;
-            debug.grounded = grounded;
-            if !grounded {
-                force.clear();
-                torque.clear();
-                continue;
-            }
-
-            debug.spring_force = spring_force;
-
-            let normal_force = spring_force.dot(normal) * normal;
-            debug.normal_force = normal_force;
-            let friction_force = normal_force.length() * GLOBAL_FRICTION;
-            let tangential_force = spring_force - normal_force;
-            debug.tangential_force = tangential_force;
-
-            let tangent_plane = normal.cross(Vec3::Y).normalize_or_zero();
-            let tangent_slope = normal.cross(tangent_plane).normalize_or_zero();
-            let tangent_z = if normal.dot(Vec3::X).abs() > 0.9999 {
-                Vec3::Z
-            } else {
-                Vec3::X.cross(normal).normalize_or_zero()
-            };
-            let tangent_x = -tangent_z.cross(normal);
-            let acc_tangent = move_state.acc_dir.x * tangent_x + move_state.acc_dir.z * tangent_z;
-            let tangent_vel = *velocity - velocity.dot(normal) * normal;
-            let prev_tangent_vel = move_state.prev_vel - move_state.prev_vel.dot(normal) * normal;
-
-            debug.tangent_vel = tangent_vel;
-
-            let target_vel = acc_tangent * 7.0;
-            debug.target_vel = target_vel;
-            let denominator = 1.0 - tangent_slope.dot(ext_dir).powi(2);
-            let slope_force = if denominator == 0.0 {
-                external_forces
-            } else {
-                -tangent_slope.dot(ext_dir) * normal_force.dot(ext_dir) * tangent_slope
-                    / denominator
-            };
-
-            // let slope_force = external_forces - normal.dot(external_forces) * normal;
-
-            let mut target_force = 0.3 * (target_vel - tangent_vel);
-            if friction_force > 0.0 {
-                target_force = (target_force.length() / friction_force).powi(1)
-                    * friction_force
-                    * target_force.normalize_or_zero()
-            }
-            target_force -= 0.0 * (tangent_vel - prev_tangent_vel) / dt.delta_seconds();
-
-            if target_force.length() > friction_force * FRICTION_MARGIN {
-                target_force = add_results_in_length(
-                    target_force.normalize_or_zero(),
-                    -slope_force,
-                    friction_force * FRICTION_MARGIN,
-                )
-                .unwrap_or(target_force)
-            }
-            target_force -= slope_force;
-            move_state.prev_vel = velocity.clone();
-            debug.target_force = target_force;
-            // println!("{:?}, {:?}", target_force, normal_force);
-
-            let target_spring_dir = (target_force + normal_force).normalize_or_zero();
-
-            let raw_neg_cast_vec = (move_state.neg_cast_vec
-                + 10.0 * (target_spring_dir - spring_dir) * dt.delta_seconds())
-            .normalize_or_zero();
-
-            let quat = Quat::from_rotation_arc(capsule_up, raw_neg_cast_vec);
-            let angle = capsule_up.angle_between(raw_neg_cast_vec);
-            move_state.neg_cast_vec = if angle < 0.0001 {
-                raw_neg_cast_vec
-            } else {
-                Quat::IDENTITY.slerp(quat, angle.min(0.3 * PI) / angle) * capsule_up
-            };
-
-            let target_up = move_state.neg_cast_vec;
-            let target_right = target_vel.cross(target_up).try_normalize().unwrap_or(
-                move_state
-                    .forward_dir
-                    .cross(target_up)
-                    .try_normalize()
-                    .unwrap(),
-            );
-            let delta_quat = Quat::from_rotation_arc(capsule_up, target_up);
-
-            debug.delta_quat = delta_quat;
-            let angular_spring_torque = angular_spring.stiffness
-                * Quat::from_rotation_arc(capsule_up, target_up).to_scaled_axis()
-                + (angular_spring.turn_stiffness
-                    * Quat::from_rotation_arc(capsule_right, target_right).to_scaled_axis())
-                - angular_spring.damping * angular_vel.clone();
-
-            debug.spring_torque = angular_spring_torque;
-
-            let angle_force = -normal.cross(angular_spring_torque) / (normal.dot(contact_point));
-
-            debug.angle_force = angle_force;
-            move_state.current_force =
-                (tangential_force + angle_force).clamp_length_max(friction_force);
-            move_state.ext_force = slope_force;
-            move_state.slipping = (tangential_force + angle_force).length() > friction_force;
-
-            force.clear();
-            force.apply_force_at_point(
-                normal_force + (tangential_force + angle_force).clamp_length_max(friction_force),
-                contact_point,
-                Vec3::ZERO,
-            );
-            torque.clear();
-            torque.apply_torque(angular_spring_torque - contact_point.cross(angle_force));
-        } else {
-            debug.grounded = false;
-            move_state.neg_cast_vec = capsule_up.try_normalize().unwrap_or(Vec3::Y);
-            move_state.contact_point = None;
-            force.clear();
-            torque.clear();
         }
     }
 }
@@ -964,15 +663,6 @@ fn draw_debug_gizmos(
     }
 }
 
-fn add_results_in_length(dir: Vec3, rhs: Vec3, combined_length: f32) -> Option<Vec3> {
-    let dot = dir.dot(rhs);
-    let discriminant = dot.powi(2) - rhs.dot(rhs) + combined_length.powi(2);
-    if discriminant < 0.0 {
-        return None;
-    }
-    Some((-dot + discriminant.sqrt()) * dir)
-}
-
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
@@ -991,13 +681,6 @@ impl Plugin for PlayerPlugin {
             max_damping: 0.0,
             max_force: 20.0 * 10.0,
             min_force: 0.0,
-            smoothing_spring: SpringValue {
-                f: 60.0,
-                zeta: 10.0,
-                m: 1.0,
-                velocity: 0.0,
-                value: 0.0,
-            },
         });
         app.insert_resource(PlayerAngularSpring {
             stiffness: 0.,
@@ -1026,7 +709,7 @@ impl Plugin for PlayerPlugin {
                 (track_camera_3rd_person, track_camera_1st_person)
                     .chain()
                     .run_if(in_state(DebugState::None)),
-                update_ground_force,
+                physics::update_ground_force,
             )
                 .chain())
             .before(IntegrationSet::Velocity),
---
+++ src/player/physics.rs
@@ -0,0 +1,324 @@
+use std::f32::consts::PI;
+
+use avian3d::prelude::*;
+use bevy::prelude::*;
+
+pub const CAPSULE_RADIUS: f32 = 0.2;
+pub const CAPSULE_HEIGHT: f32 = 4.0 * CAPSULE_RADIUS;
+pub const CAST_RADIUS: f32 = 1.0 * CAPSULE_RADIUS;
+pub const MAX_TOI: f32 = CAPSULE_HEIGHT * 1.0;
+pub const FRICTION_MARGIN: f32 = 0.9;
+pub const GLOBAL_FRICTION: f32 = 1.0;
+
+fn add_results_in_length(dir: Vec3, rhs: Vec3, combined_length: f32) -> Option<Vec3> {
+    let dot = dir.dot(rhs);
+    let discriminant = dot.powi(2) - rhs.dot(rhs) + combined_length.powi(2);
+    if discriminant < 0.0 {
+        return None;
+    }
+    Some((-dot + discriminant.sqrt()) * dir)
+}
+
+#[derive(Clone)]
+enum SpringParams {
+    Physical { stiffness: f32, damping: f32 },
+    Effective { f: f32, zeta: f32, m: f32 },
+}
+
+impl SpringParams {
+    fn get_physical(&self) -> Self {
+        match self {
+            Self::Physical {
+                stiffness: _,
+                damping: _,
+            } => self.clone(),
+            Self::Effective { f, zeta, m } => Self::Physical {
+                stiffness: (2.0 * PI * f).powi(2) * m,
+                damping: zeta * f * m / PI,
+            },
+        }
+    }
+}
+
+#[derive(Component, Reflect, Debug, Clone, Default)]
+struct SpringValue {
+    f: f32,
+    zeta: f32,
+    m: f32,
+    velocity: f32,
+    value: f32,
+}
+
+impl SpringValue {
+    fn update(&mut self, target: f32, dt: f32) -> f32 {
+        let k = (2.0 * PI * self.f).powi(2) * self.m;
+        let c = self.zeta * self.f * self.m / PI;
+        self.velocity += k * (target - self.value) * dt - c * self.velocity * dt;
+        self.value += self.velocity * dt;
+        self.value
+    }
+}
+
+#[derive(Component, Reflect, Debug, Resource, Clone, Default)]
+#[reflect(Component, Resource)]
+pub struct PlayerGroundSpring {
+    pub rest_length: f32,
+    pub stiffness: f32,
+    pub min_damping: f32,
+    pub max_damping: f32,
+    pub max_force: f32,
+    pub min_force: f32,
+}
+
+impl PlayerGroundSpring {
+    fn force(&mut self, length: f32, vel: f32, normal: Vec3, dt: f32) -> f32 {
+        let damping = self
+            .max_damping
+            .lerp(self.min_damping, normal.dot(Vec3::Y).abs());
+        let target = (-self.stiffness * (length - self.rest_length).min(0.0) - damping * vel)
+            .min(self.max_force)
+            .max(self.min_force);
+        target
+    }
+}
+
+#[derive(Component, Reflect, Debug, Resource, Clone, Default)]
+#[reflect(Component, Resource)]
+pub struct PlayerAngularSpring {
+    pub stiffness: f32,
+    pub damping: f32,
+    pub turn_stiffness: f32,
+}
+
+#[derive(Component, Reflect, Debug, Default)]
+pub struct PhysicsDebugInfo {
+    pub grounded: bool,
+    pub spring_force: Vec3,
+    pub spring_torque: Vec3,
+    pub normal_force: Vec3,
+    pub tangential_force: Vec3,
+    pub shape_toi: f32,
+    pub cast_dir: Vec3,
+    pub spring_dir: Vec3,
+    pub contact_point: Vec3,
+    pub position: Vec3,
+    pub angle_force: Vec3,
+    pub ground_normal: Vec3,
+    pub tangent_vel: Vec3,
+    pub target_vel: Vec3,
+    pub target_force: Vec3,
+    pub delta_quat: Quat,
+}
+
+#[derive(Component, Reflect, Debug, Default)]
+pub struct PlayerMoveState {
+    pub acc_dir: Vec3,
+    pub spring_height: f32,
+    prev_vel: Vec3,
+    prev_angular_force: Vec3,
+    prev_target_force: Vec3,
+    neg_cast_vec: Vec3,
+    pub slipping: bool,
+    pub contact_point: Option<Vec3>,
+    pub contact_normal: Option<Vec3>,
+    pub current_force: Vec3,
+    pub ext_force: Vec3,
+    pub forward_dir: Vec3,
+}
+
+impl PlayerMoveState {
+    pub fn new() -> Self {
+        Self {
+            neg_cast_vec: Vec3::Y,
+            ..Default::default()
+        }
+    }
+}
+
+pub fn update_ground_force(
+    mut query: Query<(
+        &mut ExternalForce,
+        &mut ExternalTorque,
+        &Position,
+        &Rotation,
+        &LinearVelocity,
+        &AngularVelocity,
+        &mut PlayerGroundSpring,
+        &PlayerAngularSpring,
+        &mut PlayerMoveState,
+        &mut PhysicsDebugInfo,
+    )>,
+    shape_cast: SpatialQuery,
+    dt: Res<Time<Substeps>>,
+    gravity: Res<Gravity>,
+) {
+    for (
+        mut force,
+        mut torque,
+        Position(position),
+        Rotation(quat),
+        LinearVelocity(velocity),
+        AngularVelocity(angular_vel),
+        mut spring,
+        angular_spring,
+        mut move_state,
+        mut debug,
+    ) in query.iter_mut()
+    {
+        let external_forces = gravity.0;
+        let ext_dir = external_forces.normalize_or_zero();
+        let filter = SpatialQueryFilter::from_mask(super::Layer::Platform);
+        let cast_dir = -move_state.neg_cast_vec;
+        let capsule_up = *quat * Vec3::Y;
+        let capsule_right = *quat * Vec3::X;
+        move_state.forward_dir = *quat * Vec3::NEG_Z;
+        if let Some(coll) = shape_cast.cast_shape(
+            &Collider::sphere(CAST_RADIUS),
+            position.clone(),
+            Quat::IDENTITY,
+            Dir3::new_unchecked(cast_dir.normalize_or_zero()),
+            MAX_TOI,
+            false,
+            filter.clone(),
+        ) {
+            let normal = coll.normal1;
+            debug.ground_normal = normal;
+            move_state.contact_normal = Some(normal);
+
+            let contact_point = coll.point2 + cast_dir * coll.time_of_impact;
+            move_state.contact_point = Some(contact_point);
+            let spring_dir = -contact_point.normalize_or_zero();
+            // let spring_dir = from_up;
+            debug.contact_point = contact_point;
+            debug.shape_toi = coll.time_of_impact;
+            debug.cast_dir = cast_dir;
+
+            let spring_vel = -velocity.dot(normal) / (-spring_dir.dot(normal));
+            // println!("Time {:?}", coll.time_of_impact);
+            let spring_force = spring.force(
+                contact_point.length(),
+                spring_vel,
+                normal,
+                dt.delta_seconds(),
+            ) * spring_dir;
+            let grounded = spring_force.length() > 0.0001;
+            debug.grounded = grounded;
+            if !grounded {
+                force.clear();
+                torque.clear();
+                continue;
+            }
+
+            debug.spring_force = spring_force;
+
+            let normal_force = spring_force.dot(normal) * normal;
+            debug.normal_force = normal_force;
+            let friction_force = normal_force.length() * GLOBAL_FRICTION;
+            let tangential_force = spring_force - normal_force;
+            debug.tangential_force = tangential_force;
+
+            let tangent_plane = normal.cross(Vec3::Y).normalize_or_zero();
+            let tangent_slope = normal.cross(tangent_plane).normalize_or_zero();
+            let tangent_z = if normal.dot(Vec3::X).abs() > 0.9999 {
+                Vec3::Z
+            } else {
+                Vec3::X.cross(normal).normalize_or_zero()
+            };
+            let tangent_x = -tangent_z.cross(normal);
+            let acc_tangent = move_state.acc_dir.x * tangent_x + move_state.acc_dir.z * tangent_z;
+            let tangent_vel = *velocity - velocity.dot(normal) * normal;
+            let prev_tangent_vel = move_state.prev_vel - move_state.prev_vel.dot(normal) * normal;
+
+            debug.tangent_vel = tangent_vel;
+
+            let target_vel = acc_tangent * 7.0;
+            debug.target_vel = target_vel;
+            let denominator = 1.0 - tangent_slope.dot(ext_dir).powi(2);
+            let slope_force = if denominator == 0.0 {
+                external_forces
+            } else {
+                -tangent_slope.dot(ext_dir) * normal_force.dot(ext_dir) * tangent_slope
+                    / denominator
+            };
+
+            // let slope_force = external_forces - normal.dot(external_forces) * normal;
+
+            let mut target_force = 0.3 * (target_vel - tangent_vel);
+            if friction_force > 0.0 {
+                target_force = (target_force.length() / friction_force).powi(1)
+                    * friction_force
+                    * target_force.normalize_or_zero()
+            }
+            target_force -= 0.0 * (tangent_vel - prev_tangent_vel) / dt.delta_seconds();
+
+            if target_force.length() > friction_force * FRICTION_MARGIN {
+                target_force = add_results_in_length(
+                    target_force.normalize_or_zero(),
+                    -slope_force,
+                    friction_force * FRICTION_MARGIN,
+                )
+                .unwrap_or(target_force)
+            }
+            target_force -= slope_force;
+            move_state.prev_vel = velocity.clone();
+            debug.target_force = target_force;
+            // println!("{:?}, {:?}", target_force, normal_force);
+
+            let target_spring_dir = (target_force + normal_force).normalize_or_zero();
+
+            let raw_neg_cast_vec = (move_state.neg_cast_vec
+                + 10.0 * (target_spring_dir - spring_dir) * dt.delta_seconds())
+            .normalize_or_zero();
+
+            let quat = Quat::from_rotation_arc(capsule_up, raw_neg_cast_vec);
+            let angle = capsule_up.angle_between(raw_neg_cast_vec);
+            move_state.neg_cast_vec = if angle < 0.0001 {
+                raw_neg_cast_vec
+            } else {
+                Quat::IDENTITY.slerp(quat, angle.min(0.3 * PI) / angle) * capsule_up
+            };
+
+            let target_up = move_state.neg_cast_vec;
+            let target_right = target_vel.cross(target_up).try_normalize().unwrap_or(
+                move_state
+                    .forward_dir
+                    .cross(target_up)
+                    .try_normalize()
+                    .unwrap(),
+            );
+            let delta_quat = Quat::from_rotation_arc(capsule_up, target_up);
+
+            debug.delta_quat = delta_quat;
+            let angular_spring_torque = angular_spring.stiffness
+                * Quat::from_rotation_arc(capsule_up, target_up).to_scaled_axis()
+                + (angular_spring.turn_stiffness
+                    * Quat::from_rotation_arc(capsule_right, target_right).to_scaled_axis())
+                - angular_spring.damping * angular_vel.clone();
+
+            debug.spring_torque = angular_spring_torque;
+
+            let angle_force = -normal.cross(angular_spring_torque) / (normal.dot(contact_point));
+
+            debug.angle_force = angle_force;
+            move_state.current_force =
+                (tangential_force + angle_force).clamp_length_max(friction_force);
+            move_state.ext_force = slope_force;
+            move_state.slipping = (tangential_force + angle_force).length() > friction_force;
+
+            force.clear();
+            force.apply_force_at_point(
+                normal_force + (tangential_force + angle_force).clamp_length_max(friction_force),
+                contact_point,
+                Vec3::ZERO,
+            );
+            torque.clear();
+            torque.apply_torque(angular_spring_torque - contact_point.cross(angle_force));
+        } else {
+            debug.grounded = false;
+            move_state.neg_cast_vec = capsule_up.try_normalize().unwrap_or(Vec3::Y);
+            move_state.contact_point = None;
+            force.clear();
+            torque.clear();
+        }
+    }
+}
